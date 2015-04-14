1622c112e01a035ae3028b8bc313c73c7392476e
Mediatrends <mauro@mediatrends.cl>
2015-04-14 17:43:20 -0300

    examples 2

--- app/wp-content/themes/Chicfit/index.php
+++ app/wp-content/themes/Chicfit/index.php
@@ -2,6 +2,7 @@
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
  	<title></title>
  	<link rel="stylesheet" href="css/chicfit.css">
  	<script src="js/chicfit.min.js"></script>
@@ -9,8 +10,8 @@
  <body>
  	<div class="container">
 		<div id="header">Header</div>
-		<div id="content">Contenido</div>
 		<div id="sidebar">Sidebar</div>  
+		<div id="content">Contenido</div>
 		<div id="footer">Footer</div> 		
  	</div>
  </body>
