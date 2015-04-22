62d02f5ca4be4e88c7eb570a236c9fca3711d131
marlenetiare <marlene.jamett@gmail.com>
2015-04-22 12:46:59 -0300

    destacados

--- app/wp-content/themes/Chicfit/loop-destacados.php
+++ app/wp-content/themes/Chicfit/loop-destacados.php
@@ -3,14 +3,17 @@
 	<?php $query = new WP_Query('posts_per_page=4&category_name=destacado'); ?>
 
 	<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-		<article class="content-destacados" id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image:url('<?php the_field('imagen_post_banner'); ?>');">
+		<article class="content-destacados" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+			<img src="<?php the_field('imagen_post_banner'); ?>" alt="" class="img-loop">
+			<h1><?php the_title(); ?></h1>
+			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 			
 			<?php the_tags('<ul class="tags"><li><span>#</span>','</li><li><span>#</span>','</li></ul>'); ?>
 
-			<a href="<?php the_permalink() ?>" class="alt_link">
-			<h1><?php the_title(); ?></h1>
-			</a>
-			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			<a href="<?php the_permalink() ?>" class="alt_link"></a>
+
+			
 
 		</article>
 
