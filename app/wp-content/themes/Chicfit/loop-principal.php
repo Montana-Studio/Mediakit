e6772c65d3d59794cf86428fc16d91b51c724977
marlenetiare <marlene.jamett@gmail.com>
2015-04-24 15:43:47 -0300

    search page

--- app/wp-content/themes/Chicfit/loop-principal.php
+++ app/wp-content/themes/Chicfit/loop-principal.php
@@ -2,10 +2,12 @@
 	
 	<div id="container" class="container-principal">
 			<div id="reciente" class="item-principal">
+				<div class="bg-color">
 					<h2>LO MAS RECIENTE</h2>
 					<nav class="tags">
 							<?php html5blank_nav(); ?>
-					</nav>			
+					</nav>	
+				</div>			
 			</div>
 			<?php 
 				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
@@ -16,16 +18,17 @@
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				
 			<article class="item-principal" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<div class="social">
-					<li><i class="fa fa-facebook"></i></li>
-					<li><i class="fa fa-twitter"></i></li>
-				</div>
-				<div class="principal-post" style="background-image:url('<?php the_field('imagen_post_banner'); ?>')">
-					<a href="<?php the_permalink()?>">
-						<h2><?php the_title(); ?></h2>
-						<div class="date"><p>11 de abril</p></div>
-					</a>
-				</div>
+					<div class="social">
+						<li><i class="fa fa-facebook"></i></li>
+						<li><i class="fa fa-twitter"></i></li>
+					</div>
+				
+					<div class="principal-post" style="background-image:url('<?php the_field('imagen_post_banner'); ?>')">
+						<a href="<?php the_permalink()?>">
+							<h2><?php the_title(); ?></h2>
+							<div class="date"><p>11 de abril</p></div>
+						</a>
+					</div>
 			</article>
 
 			<?php endwhile;
@@ -35,11 +38,12 @@
 			<?php endif; ?>
 
 			<div class="item-principal paginador">
-				<h2>VER MÁS</h2>
-				<div class="pages">
-					<?php get_template_part('pagination'); ?>
+				<div class="bg-color">
+					<h2>VER MÁS</h2>
+					<div class="pages">
+						<?php get_template_part('pagination'); ?>
+					</div>
 				</div>
-				
 			</div>
 	</div>
 
