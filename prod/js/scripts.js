87afe9baacd0fe5e3c21663985f19aa9223b6e85
marlenetiare <marlene.jamett@gmail.com>
2015-05-06 14:50:13 -0400

    update 06/05 cambios css

--- prod/js/scripts.js
+++ prod/js/scripts.js
@@ -48,7 +48,7 @@ var feed = new Instafeed({
     $('.pestana').swipe( {
         swipeDown:function() {
           $('.content-menu').animate({
-                height: 360,
+                height: 271,
                 'padding-bottom':50
             },{duration:500});
         },
