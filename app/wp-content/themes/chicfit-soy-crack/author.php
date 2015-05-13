bed99fdc4728d58c9fbf01d6e64f87bb9511f610
marlenetiare <marlene.jamett@gmail.com>
2015-05-13 13:34:16 -0400

    update 13/05 cambios css

--- app/wp-content/themes/chicfit-soy-crack/author.php
+++ app/wp-content/themes/chicfit-soy-crack/author.php
@@ -1,93 +1,101 @@
 <?php get_header(); ?>
-
-<main role="main" class="all-content">
+<section class="page-content">
 	
-	<section class="wrapper-index">
+	<main role="main" class="all-content">
+		
+		<section class="wrapper-index">
 
-		<?php if (have_posts()): the_post(); ?>
+			<?php if (have_posts()): the_post(); ?>
 
-			<h1><?php _e( 'Archivos creados por ', 'html5blank' ); ?> <?php echo get_the_author(); ?></h1>
+				<h1><?php _e( 'Archivos creados por ', 'html5blank' ); ?> <?php echo get_the_author(); ?></h1>
 
-		<?php if ( get_the_author_meta('description')) : ?>
+			<?php if ( get_the_author_meta('description')) : ?>
 
-		<?php echo get_avatar(get_the_author_meta('user_email')); ?>
+			<?php echo get_avatar(get_the_author_meta('user_email')); ?>
 
-			<h2><?php _e( 'About ', 'html5blank' ); echo get_the_author() ; ?></h2>
+				<h2><?php _e( 'About ', 'html5blank' ); echo get_the_author() ; ?></h2>
 
-			<?php echo wpautop( get_the_author_meta('description') ); ?>
+				<?php echo wpautop( get_the_author_meta('description') ); ?>
 
-		<?php endif; ?>
+			<?php endif; ?>
 
-		<?php rewind_posts(); while (have_posts()) : the_post(); ?>
+			<?php rewind_posts(); while (have_posts()) : the_post(); ?>
 
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="post-loop">
-			
-			<!-- post thumbnail -->
-			<a href="<?php the_permalink()?>">
-				<div class="img-post" style="background-image:url('<?php the_field('imagen_post_banner'); ?>')"></div>
-			</a>
-			<!-- /post thumbnail -->
-
-			<!-- post post -->
-			<div class="content-post">
-				<h2>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<p><?php html5wp_excerpt('html5wp_custom_post'); // Build your custom callback length in functions.php ?></p>
-				<div class="content-author">
-					<div class="img-author">
-						<?php echo get_avatar(get_the_author_meta()); ?>
-					</div>
-
-					<div class="name-author">
-						<?php the_author_posts_link(); ?>
-					</div>
-								
-				</div>
-			</div>
-			<!-- /post post -->
-
-			<!-- post details -->
-			<div class="date-post">
-				<div id="flecha"></div>
-				<span class="date">
-					<time datetime="<?php the_time('d/m'); ?>">
-						<p class="dia"><?php echo get_the_date('d'); ?> </p>
-						<p class="mes"><?php echo get_the_date('F'); ?> </p>
-					</time>
-				</span>
-			</div>
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div class="post-loop">
 				
-			<!-- /post details -->
+				<!-- post thumbnail -->
+				<a href="<?php the_permalink()?>">
+					<div class="img-post" style="background-image:url('<?php the_field('imagen_post_banner'); ?>')"></div>
+				</a>
+				<!-- /post thumbnail -->
 
-		</div>
-	</article>
-	<!-- /article -->
+				<!-- post post -->
+				<div class="content-post">
+					<h2>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+					</h2>
+					<p><?php html5wp_excerpt('html5wp_custom_post'); // Build your custom callback length in functions.php ?></p>
+					<div class="content-author">
+						<div class="img-author">
+							<?php echo get_avatar(get_the_author_meta()); ?>
+						</div>
 
-		<?php endwhile; ?>
+						<div class="name-author">
+							<?php the_author_posts_link(); ?>
+						</div>
+									
+					</div>
+				</div>
+				<!-- /post post -->
 
-		<?php else: ?>
+				<!-- post details -->
+				<div class="date-post">
+					<div id="flecha"></div>
+					<span class="date">
+						<time datetime="<?php the_time('d/m'); ?>">
+							<div class="icon">
+								<i class="fa fa-calendar"></i>
+							</div>
+							<div class="info-date">
+								<p class="dia"><?php echo get_the_date('d'); ?> </p>
+								<p class="mes"><?php echo get_the_date('F'); ?> </p>
+							</div>
+						</time>
+					</span>
+				</div>
+					
+				<!-- /post details -->
 
-			<!-- article -->
-			<article>
+			</div>
+		</article>
+		<!-- /article -->
 
-				<h2>No hay post.</h2>
+			<?php endwhile; ?>
 
-			</article>
-			<!-- /article -->
+			<?php else: ?>
 
-		<?php endif; ?>
+				<!-- article -->
+				<article>
 
-			<?php get_template_part('pagination'); ?>
+					<h2>No hay post.</h2>
 
-		</section>
-		<!-- /section -->
+				</article>
+				<!-- /article -->
 
-		<?php get_sidebar(); ?>
-	
-	</main>
+			<?php endif; ?>
+
+				<?php get_template_part('pagination'); ?>
+
+			</section>
+			<!-- /section -->
+
+			<?php get_sidebar(); ?>
+		
+		</main>	
+</section>
+
 
 
 
--- app/wp-content/themes/chicfit-soy-crack/category.php
+++ app/wp-content/themes/chicfit-soy-crack/category.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
-
-<main role="main" class="all-content">
+<section class="page-content">
+	<main role="main" class="all-content">
 	
 	<section class="wrapper-index">
 
@@ -42,8 +42,13 @@
 				<div id="flecha"></div>
 				<span class="date">
 					<time datetime="<?php the_time('d/m'); ?>">
-						<p class="dia"><?php echo get_the_date('d'); ?> </p>
-						<p class="mes"><?php echo get_the_date('F'); ?> </p>
+						<div class="icon">
+							<i class="fa fa-calendar"></i>
+						</div>
+						<div class="info-date">
+							<p class="dia"><?php echo get_the_date('d'); ?> </p>
+							<p class="mes"><?php echo get_the_date('F'); ?> </p>
+						</div>
 					</time>
 				</span>
 			</div>
@@ -73,6 +78,9 @@
 
 		
 	</main>
+	
+</section>
+
 
 
 
