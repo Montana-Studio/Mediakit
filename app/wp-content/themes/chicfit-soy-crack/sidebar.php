12e2c194044982cf7d42c02bfa13c2feba29b4f3
argkont <kohacaelo@gmail.com>
2015-05-29 13:34:01 -0300

    update 29/05 fix all

--- app/wp-content/themes/chicfit-soy-crack/sidebar.php
+++ app/wp-content/themes/chicfit-soy-crack/sidebar.php
@@ -1,6 +1,6 @@
 <!-- sidebar -->
-<aside class="sidebar" role="complementary">
-
+<aside class="sidebar" role="complementary" style="text-align:center;">
+	
 	<!-- /92947493/300X250_CFD -->
 	<div id='div-gpt-ad-1430935394858-4' style='width:300px;margin:0 auto;padding:5px 0;display:block;'>
 		<script type='text/javascript'>
@@ -23,5 +23,9 @@
 		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
 	</div>
 
+	<div class="fb-page" data-href="https://www.facebook.com/chicfitdaily" data-width="300" data-hide-cover="false" data-show-facepile="false" data-show-posts="true"></div>
+
+	<a class="twitter-timeline" width="300" href="https://twitter.com/chicfitdaily" data-widget-id="598543529464434688" style="display:inline-block;width:300px;">Tweets por el @chicfitdaily.</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </aside>
 <!-- /sidebar -->
