0496de95fc8fa6895f0aa7cf286370f35b167e0b
argkont <kohacaelo@gmail.com>
2015-07-15 11:10:07 -0300

    update 15/07 login page

--- app/wp-content/themes/Mediakit/functions.php
+++ app/wp-content/themes/Mediakit/functions.php
@@ -390,7 +390,7 @@ function create_post_type_html5()
 // Custom Login
 function my_login_stylesheet() {
     wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/css/mediakit.css' );
-    wp_enqueue_script( 'custom-login', get_template_directory_uri() . '/style-login.js' );
+    //wp_enqueue_script( 'custom-login', get_template_directory_uri() . '/style-login.js' );
 }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
 function my_login_logo() { ?>
@@ -400,7 +400,7 @@ function my_login_logo() { ?>
         }
     </style>
 <?php }
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
+add_action('login_enqueue_scripts', 'my_login_logo' );
 /*------------------------------------*\
     ShortCode Functions
 \*------------------------------------*/
