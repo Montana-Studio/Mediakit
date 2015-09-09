9e767663be364ec775375045bce2da64f4ffc746
marlenetiare <marlene.jamett@gmail.com>
2015-09-09 15:31:50 -0300

    version prueba

--- app/wp-config.php
+++ app/wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'mediakit-test');
+define('DB_NAME', 'mediakit-09-09');
 
 /** MySQL database username */
 define('DB_USER', 'root');
