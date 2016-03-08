3abf955bde791da3513a08ca57917a125fe3661a
argkont <kohacaelo@gmail.com>
2016-03-08 15:53:16 -0300

    update 08/03 wordpress update - install woocommerce

--- app/wp-content/themes/Mediakit/functions.php
+++ app/wp-content/themes/Mediakit/functions.php
@@ -437,4 +437,26 @@ function cc_mime_types($mimes) {
  return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+
+//WooeCommerce Hooks
+
+//remove
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+//add
+add_action( 'after_setup_theme', 'woocommerce_support' );
+
+function my_theme_wrapper_start() {
+  echo '<section id="main">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
+
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
 ?>
