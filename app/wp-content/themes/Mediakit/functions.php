87a2fe5f805f16051d2459fc564bbbc7171f0576
Marlene Jamett <marlenejamett@marlenejamett.local>
2016-06-10 15:35:17 -0300

    update css woocomerce

--- app/wp-content/themes/Mediakit/functions.php
+++ app/wp-content/themes/Mediakit/functions.php
@@ -491,4 +491,15 @@ add_filter ( 'woocommerce_product_thumbnails_columns', 'xx_thumb_cols' );
  function xx_thumb_cols() {
      return 4; // .last class applied to every 4th thumbnail
  }
+
+//WOOCOMMERCE REMOVE
+// Remove each style one by one
+add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
+function jk_dequeue_styles( $enqueue_styles ) {
+    //unset( $enqueue_styles['woocommerce-general'] );  // Remove the gloss
+    unset( $enqueue_styles['woocommerce-layout'] );     // Remove the layout
+    unset( $enqueue_styles['woocommerce-smallscreen'] );    // Remove the smallscreen optimisation
+    return $enqueue_styles;
+}
+ 
 ?>
